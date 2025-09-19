9a68ef74257c2be3ce53527f007efbd6c7c97c4f
vargaferenc <varga0725@gmail.com>
2025-09-19 16:35:23 +0200

    [dyad] Nem használt Header komponens eltávolítása - wrote 1 file(s), deleted 1 file(s)

--- components/Header.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-// This component is no longer used in the new design.
-// It is kept in the file system but will not be rendered by the application.
-const Header: React.FC = () => {
-  return null;
-};
-
-export { Header };
